e0bc93264bbb07913559383556f9582ea6b36951
Muskanbalaiwar <iammuskangupta88@gmail.com>
2023-03-16 16:08:05 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,16 @@
+var func=(a,b)=>{return a*b};
+console.log(func(3,2));
+
+
+let student=
+{
+Name:'Muskan',
+age:30,
+print(){
+  console.log(`student name is ${this.Name}`);
+  console.log(`student age is ${this.age}`);
+}
+
+}
+
+student.print();
