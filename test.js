1733bd310c0990f985fbb5b3724557ee47b4812d
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-03-17 15:25:13 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,6 +1,23 @@
+
 const http=require('http');
 const server =http.createServer((req,res)=>{
-  console.log(`muskan gupta ${req}`);
+  const url=req.url;
+ if(url==='/home'){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Welcome home');
+  console.log('home')
+  res.end();
+ }
+ if(url==='/about'){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write('Welcome to About Us page');
+  res.end();
+ }
+ if(url==='/node'){
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(' Welcome to my Node Js project');
+  res.end();
+ }
 
 });
 
