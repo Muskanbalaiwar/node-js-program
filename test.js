8a5f1a0b49e9da527e9f3ed4c099ac895111d70a
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-03-19 16:35:06 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,14 +1,31 @@
 
 
 const express=require('express')
+const bodyparser=require('body-parser')
+
+
 const app=express();
+
+app.use(bodyparser.urlencoded({extended:false}))
 app.use((req,res,next)=>{
 
     console.log('Inside the Middleware');
     next();
 })
 
-app.use((req,res,next)=>{
+app.use('/add-product',(req,res,next)=>{
+
+    console.log('Inside the  Middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><br><input type="number" name="size"><br><br><button type="submit">Add Products</button></form>');
+    //next();
+})
+
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
 
     console.log('Inside the another Middleware');
     res.send('<h1>hello my first Express Progm</h1>');
