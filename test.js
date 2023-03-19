e974c1582e6f76bb619dbd404392ab3187bcea59
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-03-19 15:54:36 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
 
-const http=require('http');
-const routes=require('./routes')
-console.log(routes.text)
-const server =http.createServer(routes.handler);
 
-server.listen(3001)
+const express=require('express')
+const app=express();
+app.use((req,res,next)=>{
+
+    console.log('Inside the Middleware');
+    next();
+})
+
+app.use((req,res,next)=>{
+
+    console.log('Inside the another Middleware');
+    res.send('<h1>hello my first Express Progm</h1>');
+    //next();
+})
+app.listen(3001)
 
 console.log('Muskan gupta')
