4dbd518debbbbba74e71e9dbc0fd6450dbb76e50
Muskanbalaiwar <125793580+Muskanbalaiwar@users.noreply.github.com>
2023-03-19 22:06:00 +0530

    Add files via upload

--- test.js
+++ test.js
@@ -1,36 +1,27 @@
 
 
 const express=require('express')
-const bodyparser=require('body-parser')
-
 
 const app=express();
 
+const adminRouter=require('./route/admin');
+
+const shopRouter=require('./route/shop');
+
+const bodyparser=require('body-parser')
+
+
+
+
 app.use(bodyparser.urlencoded({extended:false}))
+
+app.use('/admin',adminRouter);
+app.use('/shop',shopRouter)
+
 app.use((req,res,next)=>{
-
-    console.log('Inside the Middleware');
-    next();
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
-app.use('/add-product',(req,res,next)=>{
-
-    console.log('Inside the  Middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><br><br><input type="number" name="size"><br><br><button type="submit">Add Products</button></form>');
-    //next();
-})
-
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/')
-})
-
-app.use('/',(req,res,next)=>{
-
-    console.log('Inside the another Middleware');
-    res.send('<h1>hello my first Express Progm</h1>');
-    //next();
-})
 app.listen(3001)
 
 console.log('Muskan gupta')
